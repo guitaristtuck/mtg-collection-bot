eaf743d34ebb727266e311605f54ebc48d05999f
Tucker Yates <jyates@om1.com>
2024-12-27 15:08:24 -0500

    pls cloudflare

--- src/mtg/providers/moxfield.rs
+++ src/mtg/providers/moxfield.rs
@@ -31,16 +31,15 @@ struct MoxfieldSearchResponse {
     data: Vec<MoxfieldSearchResult>
 }
 
-pub async fn search(discord_user: String, collection_id: String, search_term: String) -> Result<Vec<SearchResultCard>, Box<dyn Error + Send + Sync>> {
-    let client = Client::builder().http1_only().build()?;
+pub async fn search(discord_user: String, collection_id: String, search_term: String) -> Result<Vec<SearchResultCard>, Box<dyn Error + Send + Sync>> {    
+    let client = Client::builder().user_agent("curl/7.81.0").build()?;
     let modified_search_term = format!("\"{}\"",search_term);
 
     log::info!("Searching moxfield collection of '{}' with collection id '{}' for term '{}'",discord_user,collection_id,search_term);
     let resp = client
-        .get(format!("https://api2.moxfield.com/v1/trade-binders/{}/search?q={}", collection_id, &modified_search_term))
-        .header(CONTENT_TYPE, "application/x-www-form-urlencoded")
+        .get(format!("http://api2.moxfield.com/v1/trade-binders/{}/search?q={}", collection_id, &modified_search_term))
+        .header(CONTENT_TYPE, "*/*")
         // Added this header to circumvent cloudflare's bot detection
-        .header("User-Agent", "CUSTOM_AGENT")
         .send()
         .await?;
 
